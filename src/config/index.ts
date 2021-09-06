247c6af132bb389e96ffc8fe0be2a6dfbaba4f9e
Terexeret <armandojrivasc@gmail.com>
2021-09-06 11:34:24 -0400

    apiUrl to aldrin desktop

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Set config defaults when creating the instance
 
 const useAxios = axios.create({
-	baseURL: 'http://192.168.253.29:5051',
+	baseURL: 'http://10.198.68.21:5051',
 	headers: { common: { token: localStorage.getItem('token') } },
 });
 axios.defaults.headers['Content-Type'] = 'application/json';
