e1bb7b437eeb588e96518934912e6d627fb8347b
Terexeret <armandojrivasc@gmail.com>
2021-09-06 10:08:45 -0400

    apiUrl to aldrin's laptop

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Set config defaults when creating the instance
 
 const useAxios = axios.create({
-	baseURL: 'http://localhost:5051',
+	baseURL: 'http://192.168.253.29:5051',
 	headers: { common: { token: localStorage.getItem('token') } },
 });
 axios.defaults.headers['Content-Type'] = 'application/json';
