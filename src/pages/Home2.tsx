2bb7fb9777ff29e88a91fcbfbfa4bce4bebcd2e0
Terexeret <armandojrivasc@gmail.com>
2021-09-08 10:26:56 -0400

    logout delete token on aplication

--- src/pages/Home2.tsx
+++ src/pages/Home2.tsx
@@ -15,9 +15,9 @@ import { createStyles, makeStyles, Theme, useTheme } from '@material-ui/core/sty
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import AccountCircle from '@material-ui/icons/AccountCircle';
+import AssignmentIcon from '@material-ui/icons/Assignment';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import AssignmentIcon from '@material-ui/icons/Assignment';
 import MailIcon from '@material-ui/icons/Mail';
 import MenuIcon from '@material-ui/icons/Menu';
 import MoreIcon from '@material-ui/icons/MoreVert';
@@ -27,16 +27,13 @@ import PeopleIcon from '@material-ui/icons/People';
 import SettingsIcon from '@material-ui/icons/Settings';
 import clsx from 'clsx';
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { FormMaldito } from '../components/formMaldito';
 import GestionUsuarios from '../components/GestionUsuarios';
 import { Aceptacion } from '../components/milpagos/Aceptacion';
-import { FormMaldito } from '../components/formMaldito';
 import luffy from '../img/itachi2.png';
 import { FinishLoading } from '../store/actions/ui';
-
-//Redux
-import { useSelector } from 'react-redux';
-import { RootState }  from '../store/store';
+import { RootState } from '../store/store';
 
 const drawerWidth = 240;
 
@@ -136,7 +133,6 @@ const useStyles = makeStyles((theme: Theme) =>
 	})
 );
 
-
 export default function Home() {
 	const dispatch = useDispatch();
 	const classes = useStyles();
@@ -152,10 +148,10 @@ export default function Home() {
 	});
 
 	useEffect(() => {
-		if(userDB){
+		if (userDB) {
 			setUser(userDB);
 		}
-	}, [userDB])
+	}, [userDB]);
 
 	const handleDrawerOpen = () => {
 		setOpen(true);
@@ -165,12 +161,12 @@ export default function Home() {
 		setOpen(false);
 	};
 	const handleMenuLogout = () => {
+		localStorage.removeItem('token');
 		dispatch(FinishLoading());
 	};
 
 	const handleListItemClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>, index: number) => {
 		setSelectedIndex(index);
-		console.log(index);
 	};
 	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 	const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -420,7 +416,7 @@ export default function Home() {
 				)}
 				{selectedIndex === 3 && <Aceptacion />}
 				{selectedIndex === 4 && <GestionUsuarios />}
-				{selectedIndex === 5 && <FormMaldito/>}
+				{selectedIndex === 5 && <FormMaldito />}
 			</main>
 		</div>
 	);
