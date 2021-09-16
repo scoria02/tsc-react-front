b6ab472d638af5a89d24f94598b43c409f655295
Terexeret <armandojrivasc@gmail.com>
2021-09-16 15:08:44 -0400

    home2 with mainMenu

--- src/pages/Home2.tsx
+++ src/pages/Home2.tsx
@@ -1,45 +1,13 @@
-import AppBar from '@material-ui/core/AppBar';
-import Avatar from '@material-ui/core/Avatar';
-import Badge from '@material-ui/core/Badge';
-// import CssBaseline from '@material-ui/core/CssBaseline';
-import Divider from '@material-ui/core/Divider';
-import Drawer from '@material-ui/core/Drawer';
-import IconButton from '@material-ui/core/IconButton';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import { createStyles, makeStyles, Theme, useTheme } from '@material-ui/core/styles';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import AssignmentIcon from '@material-ui/icons/Assignment';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import HomeIcon from '@material-ui/icons/Home';
-import MailIcon from '@material-ui/icons/Mail';
-import MenuIcon from '@material-ui/icons/Menu';
-import MoreIcon from '@material-ui/icons/MoreVert';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import WorkIcon from '@material-ui/icons/Work';
-import clsx from 'clsx';
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, Route, useHistory } from 'react-router-dom';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import React from 'react';
+import { Route } from 'react-router-dom';
 import { FormMaldito } from '../components/formMaldito';
 import GestionUsuarios from '../components/GestionUsuarios';
+import MainMenu from '../components/MainMenu';
 import { Aceptacion } from '../components/milpagos/Aceptacion';
-import luffy from '../img/itachi2.png';
 import { baseUrl, urlAceptacion, urlFM } from '../routers/url';
-import { FinishLoading } from '../store/actions/ui';
-//Redux
-import { RootState } from '../store/store';
 //Components
 import Inicio from './Home';
-
-const drawerWidth = 240;
 
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
@@ -49,402 +17,46 @@ const useStyles = makeStyles((theme: Theme) =>
 			// 	margin: theme.spacing(1),
 			// },
 		},
-		grow: {
-			flexGrow: 1,
-		},
-		appBar: {
-			zIndex: theme.zIndex.drawer + 1,
-			transition: theme.transitions.create(['width', 'margin'], {
-				easing: theme.transitions.easing.sharp,
-				duration: theme.transitions.duration.leavingScreen,
-			}),
-		},
-		appBarShift: {
-			marginLeft: drawerWidth,
-			width: `calc(100% - ${drawerWidth}px)`,
-			transition: theme.transitions.create(['width', 'margin'], {
-				easing: theme.transitions.easing.sharp,
-				duration: theme.transitions.duration.enteringScreen,
-			}),
-		},
-		menuButton: {
-			marginRight: 36,
-		},
-		title: {
-			display: 'none',
-			[theme.breakpoints.up('sm')]: {
-				display: 'block',
-			},
-		},
-		hide: {
-			display: 'none',
-		},
-		drawer: {
-			width: drawerWidth,
-			flexShrink: 0,
-			whiteSpace: 'nowrap',
-		},
-		drawerOpen: {
-			width: drawerWidth,
-			transition: theme.transitions.create('width', {
-				easing: theme.transitions.easing.sharp,
-				duration: theme.transitions.duration.enteringScreen,
-			}),
-		},
-		drawerClose: {
-			transition: theme.transitions.create('width', {
-				easing: theme.transitions.easing.sharp,
-				duration: theme.transitions.duration.leavingScreen,
-			}),
-			overflowX: 'hidden',
-			width: theme.spacing(7) + 1,
-			[theme.breakpoints.up('sm')]: {
-				width: theme.spacing(9) + 1,
-			},
-		},
-		toolbar: {
-			display: 'flex',
-			alignItems: 'center',
-			justifyContent: 'flex-end',
-			padding: theme.spacing(0, 1),
-			// necessary for content to be below app bar
-			...theme.mixins.toolbar,
-		},
 		content: {
 			flexGrow: 1,
 			padding: theme.spacing(3),
-		},
-		sectionDesktop: {
-			display: 'none',
-			[theme.breakpoints.up('md')]: {
-				display: 'flex',
-			},
-		},
-		sectionMobile: {
-			display: 'flex',
-			[theme.breakpoints.up('md')]: {
-				display: 'none',
-			},
-		},
-		userName: {
-			display: 'none',
-			padding: '1rem',
-			paddingRight: '0',
-			[theme.breakpoints.up('sm')]: {
-				display: 'block',
-			},
+			marginTop: 64,
 		},
 	})
 );
 
 export default function Home() {
-	const dispatch = useDispatch();
 	const classes = useStyles();
-	const history = useHistory();
-	const theme = useTheme();
-	const menuId = 'primary-search-account-menu';
-	const mobileMenuId = 'primary-search-account-menu-mobile';
 
-	const [open2] = React.useState(true);
-	const [open, setOpen] = React.useState(false);
 	const [selectedIndex, setSelectedIndex] = React.useState(0);
-	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-	const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState<null | HTMLElement>(null);
-	const [user, setUser] = React.useState({
-		name: '',
-		last_name: '',
-	});
-
-	const isMenuOpen = Boolean(anchorEl);
-	const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-	const userDB: any = useSelector((state: RootState) => state.auth.user);
-
-	useEffect(() => {
-		if (userDB) {
-			setUser(userDB);
-		}
-	}, [userDB]);
-
-	const handleDrawerOpen = () => {
-		setOpen(true);
-	};
-
-	const handleDrawerClose = () => {
-		setOpen(false);
-	};
-	const handleMenuLogout = () => {
-		localStorage.removeItem('token');
-		dispatch(FinishLoading());
-	};
-
-	const handleListItemClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>, index: number) => {
-		if (index === 3) {
-			history.push(urlAceptacion);
-		}
-		if (index === 5) {
-			history.push(urlFM);
-		}
-		setSelectedIndex(index);
-	};
-
-	const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-		setAnchorEl(event.currentTarget);
-	};
-
-	const handleMobileMenuClose = () => {
-		setMobileMoreAnchorEl(null);
-	};
-
-	const handleMenuClose = () => {
-		setAnchorEl(null);
-		handleMobileMenuClose();
-	};
-
-	const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-		setMobileMoreAnchorEl(event.currentTarget);
-	};
-
-	const renderMenu = (
-		<Menu
-			anchorEl={anchorEl}
-			anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-			id={menuId}
-			keepMounted
-			transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-			open={isMenuOpen}
-			onClose={handleMenuClose}>
-			<MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-			<MenuItem onClick={handleMenuClose}>My account</MenuItem>
-			<MenuItem onClick={handleMenuLogout}>Salir</MenuItem>
-		</Menu>
-	);
-
-	const renderMobileMenu = (
-		<Menu
-			anchorEl={mobileMoreAnchorEl}
-			anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-			id={mobileMenuId}
-			keepMounted
-			transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-			open={isMobileMenuOpen}
-			onClose={handleMobileMenuClose}>
-			<MenuItem>
-				<IconButton aria-label='show 4 new mails' color='inherit'>
-					<Badge badgeContent={4} color='secondary'>
-						<MailIcon />
-					</Badge>
-				</IconButton>
-				<p>Messages</p>
-			</MenuItem>
-			<MenuItem>
-				<IconButton aria-label='show 11 new notifications' color='inherit'>
-					<Badge badgeContent={11} color='secondary'>
-						<NotificationsIcon />
-					</Badge>
-				</IconButton>
-				<p>Notifications</p>
-			</MenuItem>
-			<MenuItem onClick={handleProfileMenuOpen}>
-				<IconButton
-					aria-label='account of current user'
-					aria-controls='primary-search-account-menu'
-					aria-haspopup='true'
-					color='inherit'>
-					{/* <AccountCircle /> */}
-
-					<Avatar alt='Remy Sharp' src={luffy} />
-				</IconButton>
-
-				<p>Profile</p>
-			</MenuItem>
-			<MenuItem onClick={handleProfileMenuOpen}>
-				<IconButton
-					aria-label='account of current user'
-					aria-controls='primary-search-account-menu'
-					aria-haspopup='true'
-					color='inherit'>
-					<AccountCircle />
-				</IconButton>
-				<p>Salir</p>
-			</MenuItem>
-		</Menu>
-	);
 
 	return (
 		<div className={classes.root}>
 			{/* <CssBaseline /> */}
-			<AppBar
-				position='fixed'
-				className={clsx(classes.appBar, {
-					[classes.appBarShift]: open,
-				})}>
-				<Toolbar>
-					<IconButton
-						color='inherit'
-						aria-label='open drawer'
-						onClick={handleDrawerOpen}
-						edge='start'
-						className={clsx(classes.menuButton, {
-							[classes.hide]: open,
-						})}>
-						<MenuIcon />
-					</IconButton>
-					<Typography className={classes.title} variant='h6' noWrap>
-						1000Pagos C.A.
-					</Typography>
-
-					<div className={classes.grow} />
-					<div className={classes.sectionDesktop}>
-						<IconButton aria-label='show 4 new mails' color='inherit'>
-							<Badge badgeContent={4} color='secondary'>
-								<MailIcon />
-							</Badge>
-						</IconButton>
-						<IconButton aria-label='show 17 new notifications' color='inherit'>
-							<Badge badgeContent={17} color='secondary'>
-								<NotificationsIcon />
-							</Badge>
-						</IconButton>
-						<Typography className={classes.userName} variant='h6' noWrap>
-							{user.name} {user.last_name}
-						</Typography>
-						<IconButton
-							edge='end'
-							aria-label='account of current user'
-							aria-controls={menuId}
-							aria-haspopup='true'
-							onClick={handleProfileMenuOpen}
-							color='inherit'>
-							{/* <AccountCircle /> */}
-
-							<Avatar alt='Remy Sharp' src={luffy} />
-						</IconButton>
-					</div>
-					<div className={classes.sectionMobile}>
-						<IconButton
-							aria-label='show more'
-							aria-controls={mobileMenuId}
-							aria-haspopup='true'
-							onClick={handleMobileMenuOpen}
-							color='inherit'>
-							<MoreIcon />
-						</IconButton>
-					</div>
-				</Toolbar>
-			</AppBar>
-			{renderMobileMenu}
-			{renderMenu}
-			<Drawer
-				variant='permanent'
-				className={clsx(classes.drawer, {
-					[classes.drawerOpen]: open,
-					[classes.drawerClose]: !open,
-				})}
-				classes={{
-					paper: clsx({
-						[classes.drawerOpen]: open,
-						[classes.drawerClose]: !open,
-					}),
-				}}>
-				<div className={classes.toolbar}>
-					<IconButton onClick={handleDrawerClose}>
-						{theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-					</IconButton>
-				</div>
-				<Divider />
-				<List>
-					<ListItem button onClick={(event) => handleListItemClick(event, 0)}>
-						<ListItemIcon>
-							<HomeIcon />
-						</ListItemIcon>
-						<ListItemText primary='Inicio' />
-					</ListItem>
-					{/*
-					{open2 && (
-						<ListItem button onClick={(event) => handleListItemClick(event, 2)}>
-							<ListItemIcon>
-								<InboxIcon />
-							</ListItemIcon>
-							<ListItemText primary='Prueba' />
-						</ListItem>
-					)}
-					*/}
-
-					<ListItem button onClick={(event) => handleListItemClick(event, 3)}>
-						<Link to={'Aceptacion'}>
-							<ListItemIcon>
-								<WorkIcon />
-							</ListItemIcon>
-						</Link>
-						<ListItemText primary='Admision' />
-					</ListItem>
-					<ListItem button onClick={(event) => handleListItemClick(event, 5)}>
-						<ListItemIcon>
-							<AssignmentIcon />
-						</ListItemIcon>
-						<ListItemText primary='Formulario de Act.' />
-					</ListItem>
-				</List>
-				<Divider />
-				{/*
-				<List>
-					{['Trash', 'Spam'].map((text, index) => (
-						<ListItem button key={text}>
-							<ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <SettingsIcon />}</ListItemIcon>
-							<ListItemText primary={text} />
-						</ListItem>
-					))}
-					<ListItem button key={'Gestion de Usuarios'} onClick={(event) => handleListItemClick(event, 4)}>
-						<ListItemIcon>
-							<PeopleIcon />
-						</ListItemIcon>
-						<ListItemText primary={'Gestion de Usuarios'} />
-					</ListItem>
-				</List>
-					*/}
-			</Drawer>
+			<MainMenu />
 			<main className={classes.content}>
-				<div className={classes.toolbar} />
-				{selectedIndex === 0 && (
-					<Route
-						path={baseUrl}
-						render={() => {
-							return <Inicio />;
-						}}
-					/>
-				)}
-
-				{selectedIndex === 2 && (
-					<Typography paragraph>
-						jkashdlkjashdklj Aldrin Mendo amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-						labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum facilisis leo vel. Risus
-						at ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus. Convallis
-						convallis tellus id interdum velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-						adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod quis
-						viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-						quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget arcu
-						dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-						sapien faucibus et molestie ac.
-					</Typography>
-				)}
-				{selectedIndex === 3 && (
-					<Route
-						path={urlAceptacion}
-						render={() => {
-							return <Aceptacion />;
-						}}
-					/>
-				)}
+				<Route
+					path={urlAceptacion}
+					exact
+					render={() => {
+						return <Aceptacion />;
+					}}
+				/>
+				<Route
+					path={urlFM}
+					exact
+					render={() => {
+						return <FormMaldito setSelectedIndex={setSelectedIndex} />;
+					}}
+				/>
 				{selectedIndex === 4 && <GestionUsuarios />}
-				{selectedIndex === 5 && (
-					<Route
-						path={urlFM}
-						render={() => {
-							return <FormMaldito setSelectedIndex={setSelectedIndex} />;
-						}}
-					/>
-				)}
+				<Route
+					exact
+					path={baseUrl}
+					render={() => {
+						return <Inicio />;
+					}}
+				/>
 			</main>
 		</div>
 	);
