6f4447d0cabeccb05b0cc53fc85676cec06e8a99
Terexeret <armandojrivasc@gmail.com>
2021-09-16 11:40:26 -0400

    Home page on page folder

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,216 +1,40 @@
+import { Theme } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
 import React from 'react';
-import clsx from 'clsx';
-import { createStyles, makeStyles, useTheme, Theme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-import { Aceptacion } from '../components/milpagos/Aceptacion';
-import SendIcon from '@material-ui/icons/Send';
+import TranredLogo from '../img/tranred-logo.png';
 
-const drawerWidth = 240;
+interface HomeInt {}
 
-const useStyles = makeStyles((theme: Theme) =>
-	createStyles({
-		root: {
-			display: 'flex',
-		},
-		appBar: {
-			zIndex: theme.zIndex.drawer + 1,
-			transition: theme.transitions.create(['width', 'margin'], {
-				easing: theme.transitions.easing.sharp,
-				duration: theme.transitions.duration.leavingScreen,
-			}),
-		},
-		appBarShift: {
-			marginLeft: drawerWidth,
-			width: `calc(100% - ${drawerWidth}px)`,
-			transition: theme.transitions.create(['width', 'margin'], {
-				easing: theme.transitions.easing.sharp,
-				duration: theme.transitions.duration.enteringScreen,
-			}),
-		},
-		menuButton: {
-			marginRight: 36,
-		},
-		hide: {
-			display: 'none',
-		},
-		drawer: {
-			width: drawerWidth,
-			flexShrink: 0,
-			whiteSpace: 'nowrap',
-		},
-		drawerOpen: {
-			width: drawerWidth,
-			transition: theme.transitions.create('width', {
-				easing: theme.transitions.easing.sharp,
-				duration: theme.transitions.duration.enteringScreen,
-			}),
-		},
-		drawerClose: {
-			transition: theme.transitions.create('width', {
-				easing: theme.transitions.easing.sharp,
-				duration: theme.transitions.duration.leavingScreen,
-			}),
-			overflowX: 'hidden',
-			width: theme.spacing(7) + 1,
-			[theme.breakpoints.up('sm')]: {
-				width: theme.spacing(9) + 1,
-			},
-		},
-		toolbar: {
-			display: 'flex',
-			alignItems: 'center',
-			justifyContent: 'flex-end',
-			padding: theme.spacing(0, 1),
-			// necessary for content to be below app bar
-			...theme.mixins.toolbar,
-		},
-		content: {
-			flexGrow: 1,
-			padding: theme.spacing(3),
-		},
-	})
-);
-
-export default function Home() {
+const useStyles = makeStyles((theme: Theme) => ({
+	base: {
+		display: 'flex',
+		width: '100%',
+		flexDirection: 'column',
+		alignItems: 'center',
+		padding: '2rem',
+	},
+	title: {
+		fontSize: 40,
+		padding: '1rem 0',
+	},
+	subtitle: {
+		fontSize: 24,
+		padding: '1rem',
+	},
+}));
+const Inicio: React.FC<HomeInt> = () => {
 	const classes = useStyles();
-	const theme = useTheme();
-	const [open, setOpen] = React.useState(false);
-	const [open2] = React.useState(true);
-	const [selectedIndex, setSelectedIndex] = React.useState(0);
-
-	const handleDrawerOpen = () => {
-		setOpen(true);
-	};
-
-	const handleDrawerClose = () => {
-		setOpen(false);
-	};
-
-	const handleListItemClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>, index: number) => {
-		setSelectedIndex(index);
-		console.log(index);
-	};
-
 	return (
-		<div className={classes.root}>
-			<CssBaseline />
-			<AppBar
-				position='fixed'
-				className={clsx(classes.appBar, {
-					[classes.appBarShift]: open,
-				})}>
-				<Toolbar>
-					<IconButton
-						color='inherit'
-						aria-label='open drawer'
-						onClick={handleDrawerOpen}
-						edge='start'
-						className={clsx(classes.menuButton, {
-							[classes.hide]: open,
-						})}>
-						<MenuIcon />
-					</IconButton>
-					<Typography variant='h6' noWrap>
-						1000Pagos C.A.
-					</Typography>
-				</Toolbar>
-			</AppBar>
-			<Drawer
-				variant='permanent'
-				className={clsx(classes.drawer, {
-					[classes.drawerOpen]: open,
-					[classes.drawerClose]: !open,
-				})}
-				classes={{
-					paper: clsx({
-						[classes.drawerOpen]: open,
-						[classes.drawerClose]: !open,
-					}),
-				}}>
-				<div className={classes.toolbar}>
-					<IconButton onClick={handleDrawerClose}>
-						{theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-					</IconButton>
+		<>
+			<div className='ed-container'>
+				<div className={classes.base}>
+					<img src={TranredLogo} style={{ width: '30%', marginLeft: '-10%' }} alt='logo tranred' />
+					<div className={classes.title}>Bienvenido al BackOffice</div>
+					<div className={classes.subtitle}>Haga click en el menu superior izquierdo para navegar</div>
 				</div>
-				<Divider />
-				<List>
-					<ListItem button onClick={(event) => handleListItemClick(event, 0)}>
-						<ListItemIcon>
-							<SendIcon />
-						</ListItemIcon>
-						<ListItemText primary='Home' />
-					</ListItem>
-					{open2 && (
-						<ListItem button onClick={(event) => handleListItemClick(event, 2)}>
-							<ListItemIcon>
-								<SendIcon />
-							</ListItemIcon>
-							<ListItemText primary='Prueba' />
-						</ListItem>
-					)}
-					<ListItem button onClick={(event) => handleListItemClick(event, 3)}>
-						<ListItemIcon>
-							<SendIcon />
-						</ListItemIcon>
-						<ListItemText primary='Aceptacion' />
-					</ListItem>
-				</List>
-				<Divider />
-				<List>
-					{['All mail', 'Trash', 'Spam'].map((text, index) => (
-						<ListItem button key={text}>
-							<ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-							<ListItemText primary={text} />
-						</ListItem>
-					))}
-				</List>
-			</Drawer>
-			<main className={classes.content}>
-				<div className={classes.toolbar} />
-				{selectedIndex === 0 && (
-					<Typography paragraph>
-						Aldrin Mendo amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-						magna aliqua. Rhoncus dolor purus non enim praesent elementum facilisis leo vel. Risus at ultrices mi
-						tempus imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus. Convallis convallis
-						tellus id interdum velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing.
-						Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh
-						cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis imperdiet
-						massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget arcu dictum varius
-						duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien faucibus et
-						molestie ac.
-					</Typography>
-				)}
-
-				{selectedIndex === 2 && (
-					<Typography paragraph>
-						jkashdlkjashdklj Aldrin Mendo amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-						labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum facilisis leo vel. Risus
-						at ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus. Convallis
-						convallis tellus id interdum velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-						adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod quis
-						viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-						quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget arcu
-						dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-						sapien faucibus et molestie ac.
-					</Typography>
-				)}
-				{selectedIndex === 3 && <Aceptacion />}
-			</main>
-		</div>
+			</div>
+		</>
 	);
-}
+};
+
+export default Inicio;
