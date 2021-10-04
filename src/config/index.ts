600eba52ccc18f428dfeeb2dd7ac18939fbf0456
Jesús R. Hernandez Vera <jhernandez@tranred.com.ve>
2021-10-04 08:03:40 -0400

    change location handle

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,10 +4,10 @@ import LRU from 'lru-cache';
 
 // Set config defaults when creating the instance
 
-export const URL = 'http://localhost';
-export const ioURL = 'ws://localhost';
-//export const URL = 'http://10.198.68.21';
-//export const ioURL = 'ws://10.198.68.21';
+//export const URL = 'http://localhost';
+//export const ioURL = 'ws://localhost';
+export const URL = 'http://10.198.68.21';
+export const ioURL = 'ws://10.198.68.21';
 export const Port = '5051';
 export const PortFiles = '6060';
 
