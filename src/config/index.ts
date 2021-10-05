be83e75db885b15099301b1f957416a3d5d65c87
Jesús R. Hernandez Vera <jhernandez@tranred.com.ve>
2021-10-05 17:05:37 -0400

    ip

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,8 +6,14 @@ import LRU from 'lru-cache';
 
 //export const URL = 'http://localhost';
 //export const ioURL = 'ws://localhost';
-export const URL = 'http://10.198.68.21';
-export const ioURL = 'ws://10.198.68.21';
+
+//QA
+export const URL = `http://10.198.73.15`; 
+export const ioURL = 'ws://10.198.73.15';
+
+//export const URL = 'http://10.198.68.21';
+//export const ioURL = 'ws://10.198.68.21';
+
 export const Port = '5051';
 export const PortFiles = '6060';
 
