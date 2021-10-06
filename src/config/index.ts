78e75181ba803a6d775699e269175d4b9fc91f2f
jesusrafaell <jesusrafaellh@gmail.com>
2021-10-05 22:39:18 -0400

    v0

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,14 +4,14 @@ import LRU from 'lru-cache';
 
 // Set config defaults when creating the instance
 
-//export const URL = 'http://localhost';
+export const URL = 'http://localhost';
 //export const ioURL = 'ws://localhost';
 
 //QA
 //export const URL = `http://10.198.73.15`; 
 //export const ioURL = 'ws://10.198.73.15';
 
-export const URL = 'http://10.198.68.21';
+//export const URL = 'http://10.198.68.21';
 export const ioURL = 'ws://10.198.68.21';
 
 export const Port = '5051';
