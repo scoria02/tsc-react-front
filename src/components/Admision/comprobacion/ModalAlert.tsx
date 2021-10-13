20ecdbed5be6162951a95bf20665397d578270d2
Terexeret <armandojrivasc@gmail.com>
2021-10-13 16:39:06 -0400

    modal alert

--- src/components/Admision/comprobacion/ModalAlert.tsx
+++ src/components/Admision/comprobacion/ModalAlert.tsx
@@ -1,51 +1,49 @@
 //modal
-import AnimationModal from '../../modals/AnimationModal';
-import ErrorOutlineIcon from '@material-ui/icons/ErrorOutlineSharp';
 import Button from '@material-ui/core/Button';
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutlineSharp';
+import AnimationModal from '../../modals/AnimationModal';
 import './modal.scss';
-
 import { useStyles } from './pasosComprobacion/styles/styles';
 
-export const ModalAlert: React.FC<any> = ({openModal, handleCloseModal , state, handleChangeI, handleIncorret, handleCancel}) => {
+export const ModalAlert: React.FC<any> = ({
+	openModal,
+	handleCloseModal,
+	state,
+	handleChangeI,
+	handleIncorret,
+	handleCancel,
+}) => {
 	const classes = useStyles();
-	return ( 
+	return (
 		<AnimationModal openModal={openModal} handleCloseModal={handleCloseModal}>
-			<div className="paperUser">
+			<div className='paperUser'>
 				<div className='container-modal' style={{ width: '100%', height: '100%', gridColumnGap: '0' }}>
 					<div className='container-item'>
-						<div className="container-top">
-							<ErrorOutlineIcon className="icons-alert" />
-							<h2>Alerta:</h2>
-							<p>Al finalizar la validacion del formulario, se enviara este mensaje al correo del cliente OJO.</p>
+						<div className='container-top'>
+							<ErrorOutlineIcon className='icons-alert' />
+							{/* <h2>Alerta:</h2> */}
+							<p className='container-text'>
+								Indique claramente las razones por la cual el recaudo NO se validó, este mensaje sera enviado por
+								correo al cliente.
+							</p>
 						</div>
-						<textarea 
-							className="textarea-alert"
-							name='msg' 
-							value={state.msg} 
-							onChange={handleChangeI} 
-						/>
+						<textarea className='textarea-alert' name='msg' value={state.msg} onChange={handleChangeI} />
 					</div>
 				</div>
 				<div className={classes.containerBtn}>
-					<Button 
-						className={classes.btnSend}
-						variant='contained'
-						color='secondary'
-						onClick={handleCancel}
-					>
+					<Button className={classes.btnSend} variant='contained' color='secondary' onClick={handleCancel}>
 						Cancelar
 					</Button>
-					<Button 
-						className={classes.btnSend} 
+					<Button
+						className={classes.btnSend}
 						variant='contained'
 						color='primary'
 						onClick={handleIncorret}
-						disabled={state.msg.length > 10 ? false : true}
-					>
+						disabled={state.msg.length > 10 ? false : true}>
 						Guardar Mensaje
 					</Button>
 				</div>
 			</div>
 		</AnimationModal>
-	)
-}
+	);
+};
