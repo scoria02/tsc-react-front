305a29c436f58f593639e028f53d4e4c9dcfaa8c
Terexeret <armandojrivasc@gmail.com>
2021-11-12 14:39:59 -0400

    diagramas updated en admision

--- src/components/Admision/index.tsx
+++ src/components/Admision/index.tsx
@@ -1,7 +1,6 @@
 import { Fab, makeStyles, Theme } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
-import React, { useContext, useEffect } from 'react';
-import { Bar } from 'react-chartjs-2';
+import React, { useContext, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { SocketContext } from '../../context/SocketContext';
 // import { SocketContext } from '../../helpers/SocketContext';
@@ -11,6 +10,7 @@ import { SolicitudesDiferidos } from '../backoffice/SolicitudesDiferidos';
 import { SolicitudesEnEspera } from '../backoffice/SolicitudesEnEspera';
 import { SolicitudesEnProceso } from '../backoffice/SolicitudesEnProceso';
 import { SolicitudesTerminadas } from '../backoffice/SolicitudesTerminadas';
+import Barra from '../diagramas/Barra';
 import { ChartTorta } from '../diagramas/ChartConfig';
 import Comprobacion from './comprobacion';
 import Diferidos from './diferidos/Diferidos';
@@ -60,37 +60,6 @@ export const useStyles = makeStyles((theme: Theme) => ({
 	},
 }));
 
-const state = {
-	labels: ['Espera', 'Proceso', 'Terminadas'],
-	datasets: [
-		{
-			label: 'Barra',
-			axis: 'x',
-
-			backgroundColor: [
-				'rgba(20, 17, 152, 0.4)',
-				'rgba(238, 99, 82, 0.4)',
-				'rgba(63, 167, 214, 0.4)',
-				'rgba(248, 249, 72, 0.4)',
-				'rgba(95, 72, 66, 0.4)',
-				'rgba(240, 162, 2, 0.4)',
-				'rgba(247, 157, 132, 0.4)',
-			],
-			borderColor: [
-				'rgb(20, 17, 152)',
-				'rgb(238, 99, 82)',
-				'rgb(63, 167, 214)',
-				'rgb(248, 249, 72)',
-				'rgb(95, 72, 66)',
-				'rgb(153, 102, 255)',
-				'rgb(247, 157, 132)',
-			],
-			borderWidth: 1,
-			data: [10, 3, 32],
-		},
-	],
-};
-
 const Admision: React.FC = () => {
 	const dispatch = useDispatch();
 	const classes = useStyles();
@@ -99,19 +68,14 @@ const Admision: React.FC = () => {
 	const { user } = useSelector((state: any) => state.auth);
 	const { socket } = useContext(SocketContext);
 
+	const [valuesChart, setvaluesChart] = useState<number[]>([]);
+	const [keyChart, setkeyChart] = useState<string[]>([]);
+	const [chartData, setChartData] = useState({});
+
 	useEffect(() => {
 		socket.emit('cliente:dashdata', user, (data: any) => {
-			console.log(data);
-			// let cols: any[] = [];
-			// let dataD: any[] = [];
-			// Object.entries(data).forEach(([key, valor], i) => {
-			// 	cols[i] = key;
-			// 	dataD[i] = valor;
-			// });
-			// setColumns(cols);
-			// setdataNew(dataD);
-			// console.log('columns', columns);
-			// console.log('dataNew', dataNew);
+			// console.log('data', data);
+			setChartData(data);
 		});
 		// socket.emit('cliente:dashdatasiempre');
 	}, [socket, user]);
@@ -127,6 +91,20 @@ const Admision: React.FC = () => {
 		socket.emit('cliente:dashdatasiempre');
 		console.log('Aqui ta el beta');
 	};
+
+	const handleUpdateChart = (chartData: any) => {
+		const col = Object.keys(chartData!);
+		let valores: any = [];
+		Object.values(chartData).forEach((val, i) => {
+			valores[i] = val;
+		});
+		setvaluesChart(valores);
+		setkeyChart(col);
+	};
+
+	useEffect(() => {
+		handleUpdateChart(chartData);
+	}, [chartData]);
 
 	return (
 		<div className={classes.admision}>
@@ -144,28 +122,11 @@ const Admision: React.FC = () => {
 
 					<div style={{ width: '40%' }}>
 						<ChartTorta />
+						{/* <Dona chartData={valuesChart} colsData={keyChart}/> */}
 					</div>
 				</div>
 				<div className={classes.row}>
-					<div style={{ width: 560, height: 200 }}>
-						<Bar
-							data={state}
-							className='canvas_prueba'
-							options={{
-								indexAxis: 'y',
-								title: {
-									display: true,
-									text: '',
-									fontSize: 2,
-								},
-								legend: {
-									display: true,
-									position: 'right',
-								},
-								responsive: true,
-							}}
-						/>
-					</div>
+					<Barra chartData={valuesChart} colsData={keyChart} />
 				</div>
 			</div>
 			<div className='cmn-divfloat'>
---
+++ src/components/diagramas/Dona.tsx
@@ -0,0 +1,76 @@
+import { Chart } from 'chart.js';
+import { FC, useEffect, useRef } from 'react';
+
+interface Props {
+	chartData: number[];
+	colsData: string[];
+}
+
+const Dona: FC<Props> = ({ chartData, colsData }) => {
+	const chartRef = useRef<Chart | null>(null);
+
+	// helper function to format chart data since you do this twice
+	const formatData = (data: number[], cols: string[]) => ({
+		labels: cols,
+		datasets: [
+			{
+				label: 'Cantidad',
+				data: data,
+				backgroundColor: [
+					'rgba(20, 17, 152, 0.4)',
+					'rgba(238, 99, 82, 0.4)',
+					'rgba(63, 167, 214, 0.4)',
+					'rgba(248, 249, 72, 0.4)',
+					'rgba(95, 72, 66, 0.4)',
+					'rgba(240, 162, 2, 0.4)',
+					'rgba(247, 157, 132, 0.4)',
+				],
+				borderColor: [
+					'rgb(20, 17, 152)',
+					'rgb(238, 99, 82)',
+					'rgb(63, 167, 214)',
+					'rgb(248, 249, 72)',
+					'rgb(95, 72, 66)',
+					'rgb(153, 102, 255)',
+					'rgb(247, 157, 132)',
+				],
+				borderWidth: 1,
+			},
+		],
+	});
+
+	// callback creates the chart on the canvas element
+	const canvasCallback = (canvas: HTMLCanvasElement | null) => {
+		if (!canvas) return;
+		const ctx = canvas.getContext('2d');
+		if (ctx) {
+			chartRef.current?.destroy();
+			chartRef.current = new Chart(ctx, {
+				type: 'doughnut',
+				data: formatData(chartData, colsData),
+				options: {
+					// animation: false,
+					responsive: true,
+				},
+			});
+		}
+	};
+
+	// effect to update the chart when props are updated
+	useEffect(() => {
+		// must verify that the chart exists
+		if (chartRef.current) {
+			chartRef.current.data = formatData(chartData, colsData);
+			chartRef.current.update();
+		}
+
+		// cleanup function - I had to remove this as it was causing errors
+		/*return () => {
+		  chartRef.current?.destroy();
+		};*/
+	}, [chartData, colsData]);
+
+	return <canvas ref={canvasCallback}></canvas>;
+};
+
+export default Dona;
