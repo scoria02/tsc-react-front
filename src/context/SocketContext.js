202ec981bd8a96dda73650808c1192f67e172c64
Jesús R. Hernandez Vera <jhernandez@tranred.com.ve>
2021-11-28 23:06:51 -0400

    fm

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -8,7 +8,8 @@ export const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
 	//const { socket, online } = useSocket(`${URL}:${PortSocket}`); //Dev
-	const { socket, online } = useSocket('http://192.168.253.29:777');
+	const { socket, online } = useSocket('http://192.168.253.29:777'); //aldrin
+	//const { socket, online } = useSocket('http://192.168.253.27:777'); //dimas
 
 	return <SocketContext.Provider value={{ socket, online }}>{children}</SocketContext.Provider>;
 };
