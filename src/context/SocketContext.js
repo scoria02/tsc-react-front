a00fe5cfca547a59879c0ed460f46cf70ed9ad3b
Terexeret <armandojrivasc@gmail.com>
2021-12-01 09:58:42 -0400

    socket apunta a 68.21

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -1,14 +1,13 @@
 /* eslint-disable no-unused-vars */
 import { createContext } from 'react';
+import { PortSocket, URL } from '../config';
 import { useSocket } from '../hooks/useSocket';
-
-import { URL, PortSocket } from '../config';
 
 export const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
-	//const { socket, online } = useSocket(`${URL}:${PortSocket}`); //Dev
-	const { socket, online } = useSocket('http://192.168.253.29:777'); //aldrin
+	const { socket, online } = useSocket(`${URL}:${PortSocket}`); //Dev
+	// const { socket, online } = useSocket('http://192.168.253.29:777'); //aldrin
 	//const { socket, online } = useSocket('http://192.168.253.27:777'); //dimas
 
 	return <SocketContext.Provider value={{ socket, online }}>{children}</SocketContext.Provider>;
