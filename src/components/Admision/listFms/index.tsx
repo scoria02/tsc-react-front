ea72899b1feb03f0806aa1fe8dcde1d6e6ba8d65
Terexeret <armandojrivasc@gmail.com>
2021-12-01 09:58:04 -0400

    listFM warnings

--- src/components/Admision/listFms/index.tsx
+++ src/components/Admision/listFms/index.tsx
@@ -1,19 +1,16 @@
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import InputBase from '@material-ui/core/InputBase';
+//Material ui
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import SearchIcon from '@material-ui/icons/Search';
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 //import { SocketContext } from '../../../context/SocketContext';
 import { CloseModalListSolic } from '../../../store/actions/ui';
-import { useStyles } from './styles';
-
 import AnimationModal from '../../modals/AnimationModal';
-
-//Material ui
-import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
-
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
+import { useStyles } from './styles';
 
 const ListFms: React.FC = () => {
 	const classes = useStyles();
@@ -32,69 +29,59 @@ const ListFms: React.FC = () => {
 		//dispatch(clean());
 	};
 
-	const handleSearching = (e:any) => {
+	const handleSearching = (e: any) => {
 		e.preventDefault();
 		setSearching(true);
-	}
+	};
 
 	return (
-		<AnimationModal 
-			openModal={modalOpenListSolic}
-			handleCloseModal={handleClose}
-		>
-				<div className={classes.root}>
-					<Paper
-						component="form"
-						style={{ width: '100%' }}
-						onSubmit={handleSearching}
-					>
-						<InputBase
-							style={{
-								padding: '10px',
-								width: '80%'
-							}}
-							placeholder="Buscar FM"
-							name='search'
-							onChange={(event: any) => setSearch(event.target.value)}
-							value={search}
-							inputProps={{ 'aria-label': '' }}
-						/>
-						<IconButton 
-							onClick={handleSearching}
-							aria-label="search">
-							<SearchIcon />
-						</IconButton>
+		<AnimationModal openModal={modalOpenListSolic} handleCloseModal={handleClose}>
+			<div className={classes.root}>
+				<Paper component='form' style={{ width: '100%' }} onSubmit={handleSearching}>
+					<InputBase
+						style={{
+							padding: '10px',
+							width: '80%',
+						}}
+						placeholder='Buscar FM'
+						name='search'
+						onChange={(event: any) => setSearch(event.target.value)}
+						value={search}
+						inputProps={{ 'aria-label': '' }}
+					/>
+					<IconButton onClick={handleSearching} aria-label='search'>
+						<SearchIcon />
+					</IconButton>
 				</Paper>
-				<Paper
-				>
-					{searching &&
+				<Paper>
+					{searching && (
 						<>
-							<Grid container spacing={4}
+							<Grid
+								container
+								spacing={4}
 								style={{
 									padding: '.5rem 1rem',
-								}}
-							>
-								<Grid item xs={12} sm container
+								}}>
+								<Grid
+									item
+									xs={12}
+									sm
+									container
 									style={{
-										cursor: 'pointer'
-									}}
-								>
-									<Grid item xs container direction="column" spacing={2}>
+										cursor: 'pointer',
+									}}>
+									<Grid item xs container direction='column' spacing={2}>
 										<Grid item xs>
-											<Typography gutterBottom variant="subtitle1" component="div">
-												<b>
-													{'Nombre Comercio: '}
-												</b>
+											<Typography gutterBottom variant='subtitle1' component='div'>
+												<b>{'Nombre Comercio: '}</b>
 												1000pagos
 											</Typography>
-											<Typography gutterBottom variant="subtitle1" component="div">
-												<b>
-													{'Nombre Cliente: '}
-												</b>
-													Armando No Rivas
+											<Typography gutterBottom variant='subtitle1' component='div'>
+												<b>{'Nombre Cliente: '}</b>
+												Armando No Rivas
 											</Typography>
-											<Typography variant="body2" color="secondary">
-												<b>{'Code: '}</b> 
+											<Typography variant='body2' color='secondary'>
+												<b>{'Code: '}</b>
 												{search}
 											</Typography>
 										</Grid>
@@ -102,7 +89,7 @@ const ListFms: React.FC = () => {
 								</Grid>
 							</Grid>
 						</>
-					}
+					)}
 				</Paper>
 			</div>
 		</AnimationModal>
