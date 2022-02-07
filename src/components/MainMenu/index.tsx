8c49e90726f64b918edd0b35fd036aa4bf77b643
Jesús R. Hernandez Vera <jhernandez@tranred.com.ve>
2022-02-07 13:20:36 -0400

    local

--- src/components/MainMenu/index.tsx
+++ src/components/MainMenu/index.tsx
@@ -11,20 +11,9 @@ import {
 	ListItemText,
 	Menu,
 	MenuItem,
+	//AppBar,
 } from '@mui/material';
 import AppBar from '@material-ui/core/AppBar';
-//import Avatar from '@material-ui/core/Avatar';
-//import Badge from '@material-ui/core/Badge';
-// import CssBaseline from '@material-ui/core/CssBaseline';
-//import Divider from '@material-ui/core/Divider';
-//import Drawer from '@material-ui/core/Drawer';
-//import IconButton from '@material-ui/core/IconButton';
-//import List from '@material-ui/core/List';
-//import ListItem from '@material-ui/core/ListItem';
-//import ListItemIcon from '@material-ui/core/ListItemIcon';
-//import ListItemText from '@material-ui/core/ListItemText';
-//import Menu from '@material-ui/core/Menu';
-//import MenuItem from '@material-ui/core/MenuItem';
 import { makeStyles, Theme, useTheme } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
