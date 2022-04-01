fdcc1a7d7c587e8b77b2ec88bee91da091adbda4
jesusrafell <jesusrafaellh@gmail.com>
2022-04-01 09:41:31 -0400

    pdf

--- src/components/utilis/images/RecPdf.tsx
+++ src/components/utilis/images/RecPdf.tsx
@@ -1,7 +1,8 @@
 import { Avatar, Button, List, ListItem, ListItemText } from '@mui/material';
 import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
-import React from 'react';
 
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
 import Rec from './Rec';
 
 import { useStyles } from '../../Admision/comprobacion/pasosComprobacion/styles/styles';
@@ -14,20 +15,58 @@ const sxStyled = {
 };
 
 const RecPdf: React.FC<any> = ({ load, setLoad, imagen }) => {
+	const url = imagen;
+
+	pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+	const [numPages, setNumPages] = useState<number>(0);
+	const [pageNumber, setPageNumber] = useState<number>(1);
+
+	function onDocumentLoadSuccess({ numPages }: any) {
+		setNumPages(numPages);
+		setPageNumber(1);
+	}
+
+	console.log(numPages);
+	console.log('actual', pageNumber);
+
+	const handlePagePdfBack = () => {
+		if (pageNumber <= 1) setPageNumber(1);
+		else setPageNumber(pageNumber - 1);
+	};
+
+	const handlePagePdfNext = () => {
+		if (numPages - 1 >= pageNumber) setPageNumber(pageNumber + 1);
+		else setPageNumber(1);
+	};
+
 	const classes = useStyles();
 	return (
 		<>
 			{imagen.split('.')[imagen.split('.').length - 1] === 'pdf' ? (
-				<List className={classes.container_imgpdf}>
-					<ListItem sx={sxStyled.imgcenter} value={imagen}>
-						<Button className={classes.link} href={imagen} target='_blank' rel='noreferrer'>
-							<Avatar>
-								<PictureAsPdfIcon />
-							</Avatar>
-							<ListItemText className={classes.itemLink} primary={imagen.split('@')[1]} />
-						</Button>
-					</ListItem>
-				</List>
+				<>
+					<Button
+						variant='contained'
+						color='secondary'
+						onClick={handlePagePdfBack}
+						disabled={pageNumber === 1 ? true : false}>
+						Back
+					</Button>
+					<Button
+						variant='contained'
+						color='secondary'
+						onClick={handlePagePdfNext}
+						disabled={pageNumber >= numPages ? true : false}>
+						Next
+					</Button>
+					<List className={classes.container_imgpdf}>
+						<ListItem sx={sxStyled.imgcenter} value={imagen}>
+							<Document file={url} onLoadSuccess={onDocumentLoadSuccess}>
+								<Page pageNumber={pageNumber} />
+							</Document>
+						</ListItem>
+					</List>
+				</>
 			) : (
 				<Rec load={load} setLoad={setLoad} imagen={imagen} />
 			)}
